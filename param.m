545165144b6e7bc8dcde2f414fc5837ef39ef319
Ni Chen <ni_chen@163.com>
2017-08-03 10:56:41 +0800

    Add files via upload

---
+++ param.m
@@ -0,0 +1,29 @@
+elePixelCount = 58;
+eleSizeX = elePixelCount;
+eleSizeY = elePixelCount;
+
+lensArrF     = 3.3e-3;       % lens array focal lenth
+lensArrPixel = 1e-3;         % lens array pixel
+
+z_scope = 70e-3:5e-3:100e-3;
+
+invalidL = 0;
+invalidR = 0;
+invalidU = 0;
+invalidD = 0;
+
+s_div        = 2;
+
+lambda      = 500e-6;    % Record wavelength  f'=62.9mm SLM_pitch = 8.1e-6
+lambda_prime = 500e-6;    % Record wavelength  f'=62.9mm SLM_pitch = 8.1e-6
+lambda_r = 633e-9;   % Red wave length
+lambda_g = 532e-9;   % Green wave length
+lambda_b = 472e-9;   % Blue wave length
+
+z_scope = 0.07:0.001:0.13;
+
+subImgPixel = 1e-3;
+projPixel_x  = subImgPixel;
+projPixel_y  = subImgPixel;
+
+SLM_pitch= 32e-6;
